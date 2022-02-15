e933b35f3d26f73850335abf99770805c9ba0a76
pabloaraujo22 <66523165+pabloaraujo22@users.noreply.github.com>
2022-02-14 22:03:58 -0300

    COnvertendo para Objeto

--- pages/api/questoes/[id].ts
+++ pages/api/questoes/[id].ts
@@ -2,5 +2,5 @@ import dbquestao from '../bancoDeQuestoes';
 export default function questoes(req, res) {
     const { id } = req.query;
 
-    res.status(200).json(dbquestao[0]);
+    res.status(200).json(dbquestao[0].paraObjeto());
 }
