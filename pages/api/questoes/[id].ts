d5be4975671736ba9ee86f42efb950b396e10dd9
pabloaraujo22 <66523165+pabloaraujo22@users.noreply.github.com>
2022-02-14 22:10:08 -0300

    Construindo API 01

--- pages/api/questoes/[id].ts
+++ pages/api/questoes/[id].ts
@@ -2,5 +2,14 @@ import dbquestao from '../bancoDeQuestoes';
 export default function questoes(req, res) {
     const { id } = req.query;
 
-    res.status(200).json(dbquestao[0].paraObjeto());
+    const unicaQuestaoOuNada = dbquestao.filter(
+        (questao) => questao.id === +id
+    );
+
+    if (unicaQuestaoOuNada.length === 1) {
+        const questaoSelecionada = unicaQuestaoOuNada[0];
+        res.status(200).json(questaoSelecionada.paraObjeto());
+    } else {
+        res.status(204).send();
+    }
 }
