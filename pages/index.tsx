0550d886bf8e1f821d3b487b1a62c18a9c0a7e80
pabloaraujo22 <66523165+pabloaraujo22@users.noreply.github.com>
2022-03-04 20:29:10 -0300

    Componente Enunciado

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,22 @@ import QuestaoModel from '../model/questao';
 import RespostaModel from '../model/resposta';
 
 export default function Home() {
-    const questaoTeste = new QuestaoModel(1, 'Melhor Cor', [
+    const questaoTeste = new QuestaoModel(1, 'Melhor Cor?', [
         RespostaModel.errada('verde'),
         RespostaModel.errada('Vermelha'),
         RespostaModel.errada('Azul'),
         RespostaModel.certa('Preto'),
     ]);
-    return <Questao valor={questaoTeste} />;
+    return (
+        <div
+            style={{
+                display: 'flex',
+                height: '100vh',
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}
+        >
+            <Questao valor={questaoTeste} />
+        </div>
+    );
 }
