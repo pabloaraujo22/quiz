42bebc6fed9d5907768e5e2c0b6ccc9b3a71fb48
pabloaraujo22 <66523165+pabloaraujo22@users.noreply.github.com>
2022-04-22 12:05:38 -0300

    Componente Estatistica

--- pages/resultado.tsx
+++ pages/resultado.tsx
@@ -1,5 +1,7 @@
 import { useRouter } from 'next/router';
-import styles from '../styles/Resposta.module.css';
+import Botao from '../components/Botao';
+import Estatistica from '../components/Estatistica';
+import styles from '../styles/Resultado.module.css';
 export default function Resultado(props) {
     const router = useRouter();
 
@@ -9,9 +11,24 @@ export default function Resultado(props) {
     return (
         <div className={styles.resultado}>
             <h1>Resultado Final</h1>
-            <div>{total}</div>
-            <div>{certas}</div>
-            <div>{`${percentual}%`}</div>
+            <div
+                style={{
+                    display: 'flex',
+                }}
+            >
+                <Estatistica texto="Perguntas" valor={total} />
+                <Estatistica
+                    texto="Certas"
+                    valor={certas}
+                    corFundo={'#9cd2a4'}
+                />
+                <Estatistica
+                    texto="Percentual"
+                    valor={`${percentual}%`}
+                    corFundo={'#DE6A33'}
+                />
+            </div>
+            <Botao href="/" texto="Tentar Novamente" />
         </div>
     );
 }
