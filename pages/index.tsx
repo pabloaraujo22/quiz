d83b1830483d32e8d671e40e01893f0d39be9683
pabloaraujo22 <66523165+pabloaraujo22@users.noreply.github.com>
2022-04-22 10:39:50 -0300

    Componente  Index #01

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,8 +11,32 @@ const questaoMock = new QuestaoModel(1, 'Melhor Cor?', [
     RespostaModel.errada('Azul'),
     RespostaModel.certa('Preto'),
 ]);
+
+const BASE_URL = 'http://localhost:3000/api';
+
 export default function Home() {
-    const [questao, setQuestao] = useState(questaoMock);
+    const [idsDasQuestoes, setIdsDasQuestoes] = useState<number[]>([]);
+    const [questao, setQuestao] = useState<QuestaoModel>(questaoMock);
+
+    async function carregarIdsDasQuestoes() {
+        const resp = await fetch(`${BASE_URL}/questionario`);
+        const idsQuestoes = await resp.json();
+        setIdsDasQuestoes(idsDasQuestoes);
+    }
+
+    async function carregarQuestao(idQuestao: number) {
+        const resp = await fetch(`${BASE_URL}/questoes/${idQuestao}`);
+        const json = await resp.json();
+    }
+
+    useEffect(() => {
+        carregarIdsDasQuestoes();
+    });
+
+    useEffect(() => {
+        idsDasQuestoes.length > 0 && carregarQuestao(idsDasQuestoes[0]);
+    }, [idsDasQuestoes]);
+
     // const questaoRef = useRef<QuestaoModel>();
 
     // useEffect(() => {
@@ -24,21 +48,11 @@ export default function Home() {
     function irPraProximoPasso() {}
 
     return (
-        <div
-            style={{
-                display: 'flex',
-                height: '100vh',
-                justifyContent: 'center',
-                flexDirection: 'column',
-                alignItems: 'center',
-            }}
-        >
-            <Questionario
-                questao={questao}
-                ultima={true}
-                questaoRespondida={questaoRespondida}
-                irPraProximoPasso={irPraProximoPasso}
-            />
-        </div>
+        <Questionario
+            questao={questao}
+            ultima={true}
+            questaoRespondida={questaoRespondida}
+            irPraProximoPasso={irPraProximoPasso}
+        />
     );
 }
