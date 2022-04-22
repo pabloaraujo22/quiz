d4edeebe578dfbfbb42db724c917df0316555168
pabloaraujo22 <66523165+pabloaraujo22@users.noreply.github.com>
2022-04-22 12:49:16 -0300

    Ajustes Build

--- pages/api/questionario/index.ts
+++ pages/api/questionario/index.ts
@@ -1,6 +1,6 @@
 import { embaralhar } from '../../../functions/arrays';
 import questoes from '../bancoDeQuestoes';
-export default function qustionario(req, res) {
+export default function questionario(req, res) {
     const ids = questoes.map((questao) => questao.id);
     res.status(200).json(embaralhar(ids));
 }
